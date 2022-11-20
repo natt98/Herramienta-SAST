f4bb67061b090f95dde55d1616bc6f9d2aec402b
natt98 <88492099+natt98@users.noreply.github.com>
2022-11-20 18:53:44 +0100

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,13 +1,11 @@
 import hashlib
 from urllib import request
 
-# Guardamos el contenido de la url en un archivo .html
 r = request.urlopen("https://es.wikipedia.org/wiki/MD5")
 with open("md5.html", "wb") as f:
     f.write(r.read())
 r.close()
 
-# Pedimos un texto para firmar y usamos la función md5 como hash
 texto_claro = input('Escribe el texto a cifrar: ')
 
 h = hashlib.md5(texto_claro.encode())
@@ -15,7 +13,6 @@ firma = h.hexdigest()
 
 print('El texto firmado es ' + firma)
 
-# Creamos una función que cuente la cantidad de números que contiene la firma
 def numeros(firma):
     cant_num = len([num for num in firma if num.isdigit()])
 
